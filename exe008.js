fac726e6f60aace2bbe822840cb26ac4a389c2ac
wand3rl3y <wand3rl3y@gmail.com>
2022-02-27 20:32:25 -0300

    Commite if and else

---
+++ exe008.js
@@ -0,0 +1,7 @@
+var vel = 110
+if (vel >=90){
+    console.log(`Velocidae do seu carro e muito alta ${vel} Km Multado`)
+}else {
+    console.log(`Velocidade aceitavel ${vel}Km `)
+}
+console.log("Dirija Com cuidade e use o cinto")
